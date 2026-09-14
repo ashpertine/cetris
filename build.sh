199c2224e3b18a75eead48828f2f8b47e790596a
Ashton Chew <ashton@greenfossil.com>
2026-09-14 17:07:14 +0800

    add basic UI

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -Wall -Wextra -o cetris ui.c -lncursesw
---
+++ ui.c
@@ -0,0 +1,153 @@
+#include <ncurses.h>
+#include <locale.h>
+#include <panel.h>
+
+#define BLOCK_COUNT 4
+
+typedef enum {
+  STRAIGHT,
+  SQUARE,
+  TSHAPE,
+  SKEW,
+  LSHAPE,
+  INVERSELSHAPE
+} Tetromino;
+
+typedef enum {
+  COLOR_PAIR_STRAIGHT = 1,
+  COLOR_PAIR_SQUARE,
+  COLOR_PAIR_TSHAPE,
+  COLOR_PAIR_SKEW,
+  COLOR_PAIR_LSHAPE,
+  COLOR_PAIR_INVERSELSHAPE
+} TetrominoColorPair;
+
+typedef struct TTShape {
+  int row;
+  int col;
+} ttshape;
+
+static ttshape STRAIGHT_LAYOUT[BLOCK_COUNT] = {
+  {0, 0}, {0, 1}, {0, 2}, {0, 3}
+};
+
+static ttshape SQUARE_LAYOUT[BLOCK_COUNT] = {
+  {0, 0}, {1, 0}, {0, 1}, {1, 1}
+};
+
+static ttshape TSHAPE_LAYOUT[BLOCK_COUNT] = {
+  {1, 0}, {1, 1}, {1, 2}, {0, 1}
+};
+
+static ttshape SKEW_LAYOUT[BLOCK_COUNT] = {
+  {0, 0}, {0, 1}, {1, 1}, {1, 2}
+};
+
+static ttshape LSHAPE_LAYOUT[BLOCK_COUNT] = {
+  {0, 0}, {0, 1}, {0, 2}, {1, 0}
+};
+
+static ttshape INVERSELSHAPE_LAYOUT[BLOCK_COUNT] = {
+  {0, 0}, {0, 1}, {0, 2}, {1, 2}
+};
+
+ttshape *get_ttshape(Tetromino tt) {
+  switch (tt) {
+  case STRAIGHT: return STRAIGHT_LAYOUT;
+  case SQUARE: return SQUARE_LAYOUT;
+  case TSHAPE: return TSHAPE_LAYOUT;
+  case SKEW: return SKEW_LAYOUT;
+  case LSHAPE: return LSHAPE_LAYOUT;
+  case INVERSELSHAPE: return INVERSELSHAPE_LAYOUT;
+  default:
+    return 0;
+    break;
+  }
+}
+
+int get_color_pair(Tetromino tt) {
+  switch(tt) {
+    case STRAIGHT: return COLOR_PAIR_STRAIGHT;
+    case SQUARE: return COLOR_PAIR_SQUARE;
+    case TSHAPE: return COLOR_PAIR_TSHAPE;
+    case SKEW: return COLOR_PAIR_SKEW;
+    case LSHAPE: return COLOR_PAIR_LSHAPE;
+    case INVERSELSHAPE: return COLOR_PAIR_INVERSELSHAPE;
+    default:
+      return 0;
+      break;
+  }
+}
+
+void wprint_tetronimo(WINDOW *local_win, int y, int x, Tetromino tt) {
+  int i = 0;
+  int col = 0;
+  int row = 0;
+  ttshape *layout = get_ttshape(tt);
+  int color_pair = get_color_pair(tt);
+    wattron(local_win, COLOR_PAIR(color_pair));
+    for(i = 0; i < BLOCK_COUNT; i++) {
+      col = layout[i].col * 2 + x;
+      row = layout[i].row + y;
+      mvwprintw(local_win, row, col, "  ");
+    }
+    wattroff(local_win, COLOR_PAIR(color_pair));
+    wrefresh(local_win);
+}
+
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+
+void init_settings(void) {
+  setlocale(LC_ALL, "");
+  initscr();			/* Start curses mode 		  */
+  curs_set(0);
+  start_color();
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+
+  init_pair(COLOR_PAIR_STRAIGHT,      COLOR_CYAN,    COLOR_CYAN);
+  init_pair(COLOR_PAIR_SQUARE,        COLOR_YELLOW,  COLOR_YELLOW);
+  init_pair(COLOR_PAIR_TSHAPE,        COLOR_MAGENTA, COLOR_MAGENTA);
+  init_pair(COLOR_PAIR_SKEW,          COLOR_GREEN,   COLOR_GREEN);
+  init_pair(COLOR_PAIR_LSHAPE,        COLOR_RED,   COLOR_RED);
+  init_pair(COLOR_PAIR_INVERSELSHAPE, COLOR_BLUE,    COLOR_BLUE);
+}
+
+int main() {
+	init_settings();
+  WINDOW *main_win;
+  int c;
+  int main_height = 20;
+	int main_width = 22;
+	int main_starty = (LINES - main_height) / 2;
+	int main_startx = (COLS - main_width) / 2;	
+  
+  printw("press q to quit");
+  refresh();
+  main_win = create_newwin(main_height, main_width, main_starty, main_startx);
+  wprint_tetronimo(main_win, 1, 1, SQUARE);
+  wprint_tetronimo(main_win, 3, 3, STRAIGHT);
+  wprint_tetronimo(main_win, 4, 4, TSHAPE);
+  wprint_tetronimo(main_win, 7, 6, LSHAPE);
+  wprint_tetronimo(main_win, 8, 2, SKEW);
+  wprint_tetronimo(main_win, 10, 4, INVERSELSHAPE);
+  while((c = wgetch(main_win)) != 'q') {
+    wrefresh(main_win);
+  }
+
+	endwin();
+	return 0;
+}
+
+WINDOW *create_newwin(int height, int width, int starty, int startx){	
+  WINDOW *local_win;
+
+	local_win = newwin(height, width, starty, startx);
+	box(local_win, 0, 0);		/* 0, 0 gives default characters
+					 * for the vertical and horizontal
+					 * lines			*/
+	wrefresh(local_win);		/* Show that box 		*/
+
+	return local_win;
+}
