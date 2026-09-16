d47a21adabb65c67afd73a4d4d7db59cdf6d50ea
ashpertine <201197604+ashpertine@users.noreply.github.com>
2026-09-16 23:40:28 +0800

    implement a somewhat functional game loop

--- ui.c
+++ ui.c
@@ -16,7 +16,7 @@ typedef enum {
   STRAIGHT,
   SQUARE,
   TSHAPE,
-  SKEW, // also known as Z
+  SKEW,        // also known as Z
   INVERSESKEW, // also known as S
   LSHAPE,
   INVERSELSHAPE
@@ -43,13 +43,18 @@ typedef struct TtShapeState {
   int x;
   int y;
   int longest_x;
+  int lowest_y;
+  int must_change; // 0 if the current shape can continue to be used, 1 if it
+                   // needs to be changed for a new shape in the next iteration
 } ttshape_state;
 
 static ttshape STRAIGHT_LAYOUT[BLOCK_COUNT] = {{0, 0}, {0, 1}, {0, 2}, {0, 3}};
 static ttshape SQUARE_LAYOUT[BLOCK_COUNT] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
 static ttshape TSHAPE_LAYOUT[BLOCK_COUNT] = {{1, 0}, {1, 1}, {1, 2}, {0, 1}};
-static ttshape SKEW_LAYOUT[BLOCK_COUNT] = {{0, 0}, {0, 1}, {1, 1}, {1, 2}}; // also known as Z
-static ttshape INVERSESKEW_LAYOUT[BLOCK_COUNT] = {{1, 0}, {1, 1}, {0, 1}, {0, 2}}; // also known as S
+static ttshape SKEW_LAYOUT[BLOCK_COUNT] = {
+    {0, 0}, {0, 1}, {1, 1}, {1, 2}}; // also known as Z
+static ttshape INVERSESKEW_LAYOUT[BLOCK_COUNT] = {
+    {1, 0}, {1, 1}, {0, 1}, {0, 2}}; // also known as S
 static ttshape LSHAPE_LAYOUT[BLOCK_COUNT] = {{0, 0}, {0, 1}, {0, 2}, {1, 0}};
 
 static ttshape INVERSELSHAPE_LAYOUT[BLOCK_COUNT] = {
@@ -101,10 +106,11 @@ int get_color_pair(Tetromino tt) {
 
 Tetromino get_rand_shape(Tetromino *prev_shape) {
   srand(time(NULL));
-  Tetromino new = (Tetromino)(rand() % (NUM_TETROMINOS + 1));
-  if(prev_shape == NULL) return new;
-  
-  while(new == *prev_shape) {
+  Tetromino new = (Tetromino)(rand() % NUM_TETROMINOS);
+  if (prev_shape == NULL)
+    return new;
+
+  while (new == *prev_shape) {
     new = (Tetromino)(rand() % (NUM_TETROMINOS + 1));
   }
 
@@ -115,6 +121,7 @@ void wprint_tetromino(WINDOW *local_win, ttshape_state *shape_state, int del) {
   int i = 0;
   int col = 0;
   int longest_col = 0;
+  int lowest_row = 0;
   int row = 0;
   ttshape *layout = get_ttshape(shape_state->current_shape);
   int color_pair = get_color_pair(shape_state->current_shape);
@@ -123,36 +130,46 @@ void wprint_tetromino(WINDOW *local_win, ttshape_state *shape_state, int del) {
 
   for (i = 0; i < BLOCK_COUNT; i++) {
     col = layout[i].col * 2 + shape_state->x;
-    longest_col = col > longest_col ? col : longest_col;
+    if (col > longest_col)
+      longest_col = col;
     row = layout[i].row + shape_state->y;
+    if (row > lowest_row)
+      lowest_row = row;
 
     refresh();
     mvwprintw(local_win, row, col, "  ");
   }
 
   shape_state->longest_x = longest_col;
+  shape_state->lowest_y = lowest_row;
   wattrset(local_win, A_NORMAL);
   wrefresh(local_win);
 }
 
-void wprint_horiz_shift_tetromino(WINDOW *local_win, ttshape_state *shape_state, int is_left,
-                                  int fact) {
+void wprint_horiz_shift_tetromino(WINDOW *local_win, ttshape_state *shape_state,
+                                  int is_left, int fact) {
   int curr_x = shape_state->x;
   // delete by resetting colors to default
   wprint_tetromino(local_win, shape_state, 1);
   if (is_left) {
     shape_state->x = curr_x - fact > 0 ? curr_x - fact : curr_x;
   } else {
-    shape_state->x = shape_state->longest_x + fact < EFFECTIVE_WIDTH ? curr_x + fact : curr_x;
+    shape_state->x = shape_state->longest_x + fact < EFFECTIVE_WIDTH
+                         ? curr_x + fact
+                         : curr_x;
   }
 
   // set new position and print
   wprint_tetromino(local_win, shape_state, 0);
 }
 
-void wprint_lower_tetrimino(WINDOW *local_win, ttshape_state *shape_state, int fact) {
+void wprint_lower_tetrimino(WINDOW *local_win, ttshape_state *shape_state,
+                            int fact) {
   int curr_y = shape_state->y;
-  if(curr_y + fact >= EFFECTIVE_HEIGHT) return;
+  if (shape_state->lowest_y + fact > EFFECTIVE_HEIGHT) {
+    shape_state->must_change = 1;
+    return;
+  }
 
   // delete by resetting colors to default
   wprint_tetromino(local_win, shape_state, 1);
@@ -184,7 +201,7 @@ void init_settings(void) {
   init_pair(COLOR_PAIR_INVERSELSHAPE, COLOR_BLUE, COLOR_BLUE);
 }
 
-WINDOW* init_main_win(void) {
+WINDOW *init_main_win(void) {
   WINDOW *main_win;
   int main_height = AREA_HEIGHT;
   int main_width = AREA_WIDTH;
@@ -205,31 +222,40 @@ void init_main_win_act(WINDOW *main_win, ttshape_state *state) {
   unsigned int tick_counter = 0;
   const int fall_interval = 20;
 
-  wprint_tetromino(main_win, state, 0);
   while (running) {
+    wprint_tetromino(main_win, state, 0);
     c = wgetch(main_win);
     switch (c) {
-      case 'q':
-        running = 0;
-        break;
-      case KEY_LEFT:
-        /* code */
-        wprint_horiz_shift_tetromino(main_win, state, 1, x_fact);
-        break;
-      case KEY_RIGHT:
-        wprint_horiz_shift_tetromino(main_win, state, 0, x_fact);
-        break;
-      case KEY_DOWN:
-        wprint_lower_tetrimino(main_win, state, y_fact);
-        break;
-      default:
-        break;
+    case 'q':
+      running = 0;
+      break;
+    case KEY_LEFT:
+      /* code */
+      wprint_horiz_shift_tetromino(main_win, state, 1, x_fact);
+      break;
+    case KEY_RIGHT:
+      wprint_horiz_shift_tetromino(main_win, state, 0, x_fact);
+      break;
+    case KEY_DOWN:
+      wprint_lower_tetrimino(main_win, state, y_fact);
+      break;
+    default:
+      break;
     }
 
     tick_counter++;
-    if(tick_counter >= (unsigned) fall_interval && state->y + y_fact < EFFECTIVE_HEIGHT) {
+    if (tick_counter >= (unsigned)fall_interval) {
       wprint_lower_tetrimino(main_win, state, y_fact);
       tick_counter = 0;
+    }
+
+    if (state->must_change) {
+      state->current_shape = get_rand_shape(NULL);
+      state->x = 1;
+      state->y = 1;
+      state->longest_x = 0;
+      state->lowest_y = 0;
+      state->must_change = 0;
     }
 
     wrefresh(main_win);
@@ -238,16 +264,15 @@ void init_main_win_act(WINDOW *main_win, ttshape_state *state) {
 
 int main() {
   init_settings();
-  ttshape_state state = {
-    .current_shape = get_rand_shape(NULL),
-    .x = 1,
-    .y = 1,
-    .longest_x = 1
-  };
+  ttshape_state state = {.current_shape = get_rand_shape(NULL),
+                         .x = 1,
+                         .y = 1,
+                         .longest_x = 0,
+                         .lowest_y = 0};
 
   printw("press q to quit");
   refresh();
-  WINDOW* main_win = init_main_win();
+  WINDOW *main_win = init_main_win();
   init_main_win_act(main_win, &state);
 
   endwin();
